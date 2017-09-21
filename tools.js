d319fcabe8c8fc35687a85fcaa53ede02eb1e127
Stanislav Vasilyev <creadome@gmail.com>
2017-09-21 12:39:39 +0800

    Добавлены сервисы

--- tools.js
+++ tools.js
@@ -9,13 +9,13 @@ $(document).ready(function(){
 		$('.boxGameInfo:first').before(data);
 	});
 
-	$.getScript('https://yastatic.net/es5-shims/0.0.2/es5-shims.min.js');
-	$.getScript('https://yastatic.net/share2/share.js', function(){
+	$.getScript('//yastatic.net/es5-shims/0.0.2/es5-shims.min.js');
+	$.getScript('//yastatic.net/share2/share.js', function(){
 		$('a#lnkGameTitle').each(function(){
 			$(this).closest('table').after('<div class="share">' +
 				'<h2>Расскажи друзьям!</h2>' +
 
-				'<div class="ya-share2" data-services="vkontakte" data-url="http://' + domain + '.en.cx' + $(this).attr('href') + '" data-title="' + $(this).text() + '" data-image="http://cdn.endata.cx/images/v2/en/promo/encounter.png" data-counter></div>' +
+				'<div class="ya-share2" data-services="vkontakte,facebook,twitter,viber,whatsapp,telegram" data-url="http://' + domain + '.en.cx' + $(this).attr('href') + '" data-title="' + $(this).text() + '" data-image="http://cdn.endata.cx/images/v2/en/promo/encounter.png"></div>' +
 			'</div>');
 		});
 	});
